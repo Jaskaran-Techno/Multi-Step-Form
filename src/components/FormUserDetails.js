fe03a2a17630fe4246464bd68dd4983e7a2e448c
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-12-13 18:45:14 +0530

    final commit

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import moduleName from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
   render() {
     return (
       <div>
-        
+        <h1>Hello from User Details</h1>
       </div>
     )
   }
